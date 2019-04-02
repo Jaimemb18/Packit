fb8cd95aeac9e528a05f1e10db6ec52ae8dee337
Jaimemb18 <jmb8534@rit.edu>
2019-04-02 21:00:07 +0000

    I tried to add the rest of the report
    I couldn't understand most of it and need to research more python stuff later

--- src/calc.py
+++ src/calc.py
@@ -1,6 +1,6 @@
+from itertools import permutations
 
-
-class bin:
+class Bin:
     def __init__(self, width,height,depth):
         self.width = width
         self.height = height
@@ -20,6 +20,8 @@ def pack3D (binWidth, binHeight, binDepth, items):
     packByWidth = false
     packByHeight = false
 
+    pivot = 0
+
     if binWidth < binHeight and binWidth < binDepth:
         packByWidth = true
     elif binHeight < binWidth and binHeight < binDepth:
@@ -31,9 +33,34 @@ def pack3D (binWidth, binHeight, binDepth, items):
         toPack=notPacked
         notPacked=[]
 
+        currentBin = new Bin(0,0,0)
 
+        rotations = permutations(toPack[0])
+        for i in rotations:
+            if currentBin.addItem(0,0,0, i):
+                break
 
+        for i in range(1, len(toPack)):
+            curItem = toPack[i]
+            fitted = false
 
+            for p in range(0,3):
+                k = 0
+
+                while k < len(currentBin.items) and not fitted:
+                    binItem = currentBin.items[k]
+                    
+                    if packByWidth:
+                        pivot = p
+                    elif packByHeight:
+                        pivot = (p+1) % 3
+                    else:
+                        pivot = (p-1) % 3
+
+                    if pivot is 0:
+                    elif pivot is 1:
+                    elif pivot is 2:
+                    else:
 
 
         if len(notPacked) <= 0:
